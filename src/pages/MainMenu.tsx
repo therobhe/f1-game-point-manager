25f0ade3de13a928933590afb8e6ffce74e00b17
robhe <rheinemann97@gmail.com>
2025-12-15 20:33:43 +0100

    update MainMenu component with new title and navigation link

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const MainMenu: React.FC = () => {
-    return (
-        <h1>Main Menu</h1>
-    )
-}
+	return (
+		<>
+			<h1>F1 2014 Point Manager</h1>
+			<Link
+				to="/tracks"
+				className="inline-block px-6 py-2 mt-4 bg-blue-600 text-white font-semibold rounded hover:bg-blue-700 transition-colors"
+			>
+				New Season
+			</Link>
+		</>
+	);
+};
