17c224be0dbe86b92d41b4518d09c770d00cc089
robhe <rheinemann97@gmail.com>
2025-12-15 20:21:44 +0100

    add routing and new pages for race management

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import './App.css'
-import { MainMenu } from "./pages/MainMenu.tsx";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { MainMenu } from './pages/MainMenu';
+import { ConfigCalendar } from './pages/ConfigCalendar';
+import { ConfigPointsystem } from './pages/ConfigPointsystem';
+import { RaceResult } from './pages/RaceResult';
+import { Standings } from './pages/Standings';
+import { Finish } from './pages/Finish';
 
 function App() {
-	
 	return (
-		<>
-			<MainMenu/>
-		</>
-	)
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={ <MainMenu/> }/>
+				<Route path="/tracks" element={ <ConfigCalendar/> }/>
+				<Route path="/points" element={ <ConfigPointsystem/> }/>
+				<Route path="/race/:raceId" element={ <RaceResult/> }/>
+				<Route path="/standings" element={ <Standings/> }/>
+				<Route path="/finish" element={ <Finish/> }/>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
---
+++ src/pages/Finish.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Finish: React.FC = () => {
+	return (
+		<h1>Finish</h1>
+	);
+};
